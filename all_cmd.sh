28e889a637c4f81daa413b3bd67e7879566718ef
Hello_World <1085372438@qq.com>
2022-11-23 15:34:29 +0800

    Update all_cmd.sh

--- all_cmd.sh
+++ all_cmd.sh
@@ -1,4 +1,4 @@
-python -u tools/train_net.py --config-file configs/base_training.yml --num-gpus 4 # base training
+python -u tools/train_net.py --config-file configs/base_training.yml --num-gpus 4
 python -u tools/ckpt_surgery.py --src1 checkpoints/base_pretraining/model_final.pth --save-dir checkpoints/model_files/ --tar-name model_pretrain_reset.pth --method remove
 python -u tools/train_net.py --config-file configs/base_hierarchical.yml --num-gpus 4
 python -u tools/train_net.py --config-file configs/2shot/2shot_novel.yml --num-gpus 4
